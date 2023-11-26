7bf8ab50e0d06e9e75d6cb27ad02cd46b8bdc02f
hyeonjae <hyeonjae0913+1@gmail.com>
2023-11-26 16:00:15 +0900

    how to implement GET request by object id using @Param

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+interface PostModel {
+  id: number;
+  name: string;
+  age: number;
+  address: string;
+}
+
+const posts: PostModel[] = [
+  {
+    id: 1,
+    name: 'lee',
+    age: 26,
+    address: 'kanagawa',
+  },
+  {
+    id: 2,
+    name: 'ishida',
+    age: 25,
+    address: 'miyagi',
+  },
+];
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  // GET /posts/:id
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return posts.find((result) => result.id === +id);
+  }
+}
