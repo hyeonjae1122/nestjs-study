46e53ef87b1bc7b057917d72ae950138584a95b9
hyeonjae <hyeonjae0913+1@gmail.com>
2023-11-26 18:10:49 +0900

    move logic in controller to service module

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,73 +3,35 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface PostModel {
-  id: number;
-  name: string;
-  age: number;
-  address: string;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    name: 'lee',
-    age: 26,
-    address: 'kanagawa',
-  },
-  {
-    id: 2,
-    name: 'ishida',
-    age: 25,
-    address: 'miyagi',
-  },
-];
-
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  //GET all
   @Get()
   getPosts() {
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
-  // GET /posts/:id
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((result) => result.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
-  // POST
   @Post()
   postPost(
     @Body('name') name: string,
     @Body('age') age: number,
     @Body('address') address: string,
   ) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      name,
-      age,
-      address,
-    };
-    posts = [...posts, post];
-    return post;
+    return this.postsService.createPost(name, age, address);
   }
 
-  // PUT
   @Put(':id')
   putPost(
     @Param('id') id: string,
@@ -77,32 +39,12 @@ export class PostsController {
     @Body('age') age?: number,
     @Body('address') address?: string,
   ) {
-    const post = posts.find((result) => result.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-    if (name) {
-      post.name = name;
-    }
-    if (age) {
-      post.age = age;
-    }
-    if (address) {
-      post.address = address;
-    }
-
-    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
-    return post;
+    return this.postsService.updatePost(+id, name, age, address);
   }
 
+  // Delete
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-    posts = posts.filter((post) => post.id !== +id);
-
-    return id;
+    return this.postsService.deletePost(id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+export interface PostModel {
+  id: number;
+  name: string;
+  age: number;
+  address: string;
+}
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    name: 'lee',
+    age: 26,
+    address: 'kanagawa',
+  },
+  {
+    id: 2,
+    name: 'ishida',
+    age: 25,
+    address: 'miyagi',
+  },
+];
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    const post = posts.find((result) => result.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+  createPost(name: string, age: number, address: string) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      name,
+      age,
+      address,
+    };
+    posts = [...posts, post];
+    return post;
+  }
+
+  updatePost(postId: number, name: string, age: number, address: string) {
+    const post = posts.find((result) => result.id === postId);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    if (name) {
+      post.name = name;
+    }
+    if (age) {
+      post.age = age;
+    }
+    if (address) {
+      post.address = address;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === postId ? post : prevPost));
+    return post;
+  }
+
+  deletePost(postId: string) {
+    const post = posts.find((post) => post.id === +postId);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    posts = posts.filter((post) => post.id !== +postId);
+
+    return postId;
+  }
+}
